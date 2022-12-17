a6b1655c3c85b1588a441a9ba601b1ab3b4d4e7c
Bami David <bbamii@outlook.com>
2022-12-17 17:08:48 +0100

    entry pages, and util functions

--- core/apis/index.js
+++ core/apis/index.js
@@ -1,32 +1,115 @@
+import { get_query_string } from "../../utils";
+
 export default {
+  auth: {
+    async login(_data) {
+      return await request(
+        "https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/sign-in",
+        { method: "post", data: JSON.stringify(_data) }
+      );
+    },
+    async signup({ accessType, ..._data }) {
+      return await request(
+        `https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/register/${accessType.toLowerCase()}`,
+        { data: JSON.stringify(_data) }
+      );
+    },
+  },
   bookings: {
-    async get() {
-      const res = await fetch("https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/bookings?limit=20&offset=1&city=lagos");
-      const data = res.json();
-      return data;
-    }
+    async get(opts) {
+      return await request(
+        "https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/bookings",
+        {
+          query: {
+            ...opts,
+            limit: 20,
+            offset: 1,
+            city: opts.city || "lagos",
+          },
+        }
+      );
+    },
+    async create(data) {
+      return await request(
+        "https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/bookings",
+        { method: "post", data: JSON.stringify(data) }
+      );
+    },
   },
   sessions: {
-    async get() {
-      // update...
-      const res = await fetch("https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/studios/2c4a230c-5085-4924-a3e1-25fb4fc5965b");
-      const data = res.json();
-      return data;
+    async get(merchant) {
+      try {
+        if (!merchant) {
+          throw new Error("Invalid merchant id");
+        }
+        return await request(
+          `https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/studios/${merchant}`
+        );
+      } catch (error) {
+        throw error;
+      }
     },
-    async create() {
-      const res = fetch("")
-    }
+    async create(merchant, data) {
+      return request(
+        `https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/studios/${merchant}`,
+        { method: "post", data: JSON.stringify(data) }
+      );
+    },
   },
   users: {
     async get_users() {
-      const res = await fetch("https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/clients?type=USER&limit=20&offset=1");
-      const data = res.json();
-      return data;
+      return await request(
+        "https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/clients?type=USER&limit=20&offset=1"
+      );
     },
-    async get_merchants() {
-      const res = await fetch("https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/clients?type=MERCHANT&limit=20&offset=1");
-      const data = res.json();
-      return data;
-    }
+    async get_merchants(opts = {}) {
+      return await request(
+        "https://stoplight.io/mocks/pipeline/pipelinev2-projects/111233856/clients",
+        {
+          query: {
+            ...opts,
+            type: "MERCHANT",
+            limit: 20,
+            offset: 1,
+          },
+        }
+      );
+    },
+  },
+};
+
+async function request(
+  _path,
+  { method = "get", data = {}, query = {}, variables = {} } = {}
+) {
+  let path = _path;
+  const regex = /[$]\w+[^//]/g;
+  let test;
+
+  while (!!(test = regex.exec(_path))) {
+    const [_string] = test;
+    const string = _string.substring(1);
+    const value = variables[string];
+
+    path = path.replace(_string, value);
+  }
+
+  try {
+    const options = {
+      method,
+      headers: {
+        "Content-Type": "application/json"
+      },
+    };
+
+    if (method.toLowerCase() !== "get") options.body = data;
+    const res = await fetch(`${path}?${get_query_string(query)}`, options);
+
+    const _data = await res.json();
+    if (_data.errors) throw new Error(_data.errors);
+
+    return _data;
+  } catch (error) {
+    throw error;
   }
 }
--- utils/index.js
+++ utils/index.js
@@ -8,3 +8,25 @@ export const withTable = (Controller) => {
   }
   return MyController;
 }
+
+export function parseQuery (str) {
+  if(str.startsWith("?")) {
+    str = str.substring(1);
+  }
+
+  const split = str.split("&");
+  return split.reduce((acc, curr) => {
+    const [key, value] = curr.split("=");
+    return {...acc, [key]: value }
+  }, {})
+}
+
+export const get_query_string = (query) => {
+  if (!query) return "";
+
+  const query_string = new URLSearchParams();
+  Object.entries(query).forEach(([key, value]) => {
+    query_string.set(key, value);
+  });
+  return query_string.toString();
+};
